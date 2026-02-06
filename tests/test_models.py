f78888d15c8498e57c3925253a909d32974050ea
connor-jason <connorejason@gmail.com>
2026-02-06 10:56:21 -0500

    hf token

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+import os
 from PIL import Image
 from local_model import query_local
 from remote_model import query_remote, client
@@ -11,6 +13,7 @@ def test_local_model():
     assert len(result) > 0
 
 
+@pytest.mark.skipif(os.getenv("HF_TOKEN") is None, reason="skipping remote model test no HF_TOKEN")
 def test_remote_model():
     img = Image.new('RGB', (100, 100), color='blue')
     result = query_remote(img, "test?", client)
