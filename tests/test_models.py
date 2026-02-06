b634622e3476721eb5080a59f3e7d55b944c973a
connor-jason <connorejason@gmail.com>
2026-02-06 10:50:08 -0500

    is this a pytest

---
+++ tests/test_models.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from local_model import query_local
+from remote_model import query_remote, client
+
+
+def test_local_model():
+    img = Image.new('RGB', (100, 100), color='red')
+    result = query_local(img, "test!")
+
+    assert isinstance(result, str)
+    assert len(result) > 0
+
+
+def test_remote_model():
+    img = Image.new('RGB', (100, 100), color='blue')
+    result = query_remote(img, "test?", client)
+
+    assert isinstance(result, str)
+    assert len(result) > 0
